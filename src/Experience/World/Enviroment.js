0c75fc3617ad9863ed8d6425b1818e1c419c1bda
esraaas2018 <esraaas2018@gmail.com>
2022-06-04 23:13:39 +0300

    1

--- src/Experience/World/Enviroment.js
+++ src/Experience/World/Enviroment.js
@@ -18,21 +18,20 @@ export default class Enviroment {
     }
 
 
-    
-    setSunLight()
-    {
+
+    setSunLight() {
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
         this.scene.add(ambientLight)
-        
+
         const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
         directionalLight.castShadow = true
         directionalLight.shadow.mapSize.set(1024, 1024)
         directionalLight.shadow.camera.far = 15
-        directionalLight.shadow.camera.left = - 7
+        directionalLight.shadow.camera.left = -7
         directionalLight.shadow.camera.top = 7
         directionalLight.shadow.camera.right = 7
-        directionalLight.shadow.camera.bottom = - 7
-        directionalLight.position.set(- 5, 5, 0)
+        directionalLight.shadow.camera.bottom = -7
+        directionalLight.position.set(-5, 5, 0)
         this.scene.add(directionalLight)
 
     }
--- src/Experience/World/ground.js
+++ src/Experience/World/ground.js
@@ -5,20 +5,19 @@ import Experience from "../Experience";
 
 export default class Ground {
     constructor() {
-        console.log('Ground')
         this.experience = new Experience()
         this.scene = this.experience.scene
 
         const floor = new THREE.Mesh(
             new THREE.PlaneGeometry(20, 20),
             new THREE.MeshStandardMaterial({
-                color: '#444444',
+                color: '#a9c388',
                 metalness: 0,
                 roughness: 0.5
             })
         )
         floor.receiveShadow = true
-        floor.rotation.x = - Math.PI * 0.5
+        floor.rotation.x = -Math.PI * 0.5
         this.scene.add(floor)
     }
     update() {
