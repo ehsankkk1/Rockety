8587d83601748ef0c13709fa9df4f82d4d67fb35
ehsan.ab101 <ehsan.ab101@gmail.com>
2022-06-04 21:08:14 +0300

    modified light & ground & added SkyDome

--- src/Experience/World/ground.js
+++ src/Experience/World/ground.js
@@ -8,7 +8,23 @@ export default class Ground {
         console.log('Ground')
         this.experience = new Experience()
         this.scene = this.experience.scene
+        this.Ground()
+    }
+    update() {
 
+    }
+    Ground() {
+        const groundGeo = new THREE.PlaneGeometry(10000, 10000);
+        const groundMat = new THREE.MeshLambertMaterial({ color: 0xffffff });
+        groundMat.color.setHSL(0.095, 1, 0.75);
+
+        const ground = new THREE.Mesh(groundGeo, groundMat);
+        ground.position.y = - 33;
+        ground.rotation.x = - Math.PI / 2;
+        ground.receiveShadow = true;
+        this.scene.add(ground);
+    }
+    Floor() {
         const floor = new THREE.Mesh(
             new THREE.PlaneGeometry(20, 20),
             new THREE.MeshStandardMaterial({
@@ -20,8 +36,5 @@ export default class Ground {
         floor.receiveShadow = true
         floor.rotation.x = - Math.PI * 0.5
         this.scene.add(floor)
-    }
-    update() {
-
     }
 }
