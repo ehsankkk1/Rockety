7a4f04dc3220fbdbe4e1ea551488fdbdc0c124ea
ehsan.ab101 <ehsan.ab101@gmail.com>
2022-06-16 11:44:34 +0300

    value screen update

--- src/Experience/World/value_screen.js
+++ src/Experience/World/value_screen.js
@@ -3,6 +3,7 @@ import Experience from "../Experience";
 import Environment from './Enviroment.js'
 
 let text;
+let height=0 ;
 export default class ValueScreen {
     constructor() {
         this.experience = new Experience()
@@ -10,15 +11,16 @@ export default class ValueScreen {
         this.environment = new Environment()
 
         text ={
-
             element: document.querySelector('.text')
-        }
-        console.log(text.element.innerHTML)
+       } 
         //this.scene.add(cube);
     }
+   
     update() {
-
-        text.element.innerHTML = "Height : 0 m <br> Speed : 0 ms <br> Pressure : 0 KG/mole";
+        height +=1;
+        document.getElementById("height").innerHTML = height ;
+        document.getElementById("speed").innerHTML = "150" ;
+        document.getElementById("pressure").innerHTML = "25" ;
         //cube.rotation.y += 0.01
     }
 }
